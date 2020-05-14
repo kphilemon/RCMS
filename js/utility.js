ee5adb259ca56013a5db68009c3061ec616a53f4
kphilemon <kphilemon0529@gmail.com>
2020-05-15 02:34:51 +0800

    Add navbar

---
+++ js/utility.js
@@ -0,0 +1,69 @@
+function storageAvailable(type) {
+    let storage;
+    try {
+        storage = window[type];
+        let x = '__storage_test__';
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    } catch (e) {
+        return e instanceof DOMException && (
+                // everything except Firefox
+            e.code === 22 ||
+            // Firefox
+            e.code === 1014 ||
+            // test name field too, because code might not be present
+            // everything except Firefox
+            e.name === 'QuotaExceededError' ||
+            // Firefox
+            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+            // acknowledge QuotaExceededError only if there's something already stored
+            (storage && storage.length !== 0);
+    }
+}
+
+function isLoggedIn() {
+    if (!storageAvailable('localStorage')) {
+        // skip checking if storage is not available
+        console.log('skipping auth... logged in')
+        return true;
+    }
+
+    let userStatus = localStorage.getItem('status');
+    if (!userStatus) {
+        console.log('status: logged out')
+        return false;
+    }
+
+    if (userStatus === 'loggedIn') {
+        console.log('status: logged in')
+        return true;
+    }
+
+    console.log('status: logged out')
+    return false;
+}
+
+function logIn() {
+    if (storageAvailable('localStorage')) {
+        localStorage.setItem('status', 'loggedIn')
+        console.log('logging in...')
+    }
+}
+
+function logOut() {
+    localStorage.removeItem('status')
+    console.log('logging out...')
+}
+
+function loadNavBar(targetId, activeNavLinkId) {
+
+    if (isLoggedIn()) {
+        $(targetId).load('../common/navbar/logged-in.html', function () {
+            $('#pages').find('.active').removeClass('active');
+            $('#pages ' + activeNavLinkId).addClass('active');
+        });
+    } else {
+        $(targetId).load('../common/navbar/logged-out.html');
+    }
+}
