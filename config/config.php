f57f31c60e1ba422ecd3b8e8dc1445a47f04f46a
kphilemon <kphilemon0529@gmail.com>
2020-06-25 12:12:11 +0800

    Add foods and enable prod environment

--- config/config.php
+++ config/config.php
@@ -1,11 +1,10 @@
 <?php
 
-// for development environment
-//ini_set('log_errors','1');
-//ini_set('error_reporting', E_ALL );
-//ini_set('display_errors', '0');
-//ini_set('display_startup_errors', '1');
-
+// for production environment
+ini_set('log_errors','1');
+ini_set('error_reporting', E_ALL );
+ini_set('display_errors', '0');
+ini_set('display_startup_errors', '1');
 
 define('TITLE_RCMS', 'RCMS');
 define('TITLE_ACTIVITY', 'Activities');
